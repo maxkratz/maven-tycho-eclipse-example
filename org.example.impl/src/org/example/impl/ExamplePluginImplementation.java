f6f75bddfb30672fec8f3fd372ef1510b953f057
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-08-04 17:13:16 +0200

    First working test project

--- org.example.impl/src/org/example/impl/ExamplePluginImplementation.java
+++ org.example.impl/src/org/example/impl/ExamplePluginImplementation.java
@@ -5,5 +5,9 @@ public class ExamplePluginImplementation {
 	public static void main(final String[] args) {
 		System.out.println("Hello World!");
 	}
+	
+	public int addTwoNumbers(final int a, final int b) {
+		return a + b;
+	}
 
 }
---
+++ org.example.tests/src/org/example/tests/ExampleTest.java
@@ -0,0 +1,37 @@
+package org.example.tests;
+
+import org.example.impl.ExamplePluginImplementation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+	ExamplePluginImplementation sut = null;
+	
+	@BeforeEach
+	public void setUp() {
+		sut = new ExamplePluginImplementation();
+	}
+	
+	@Test
+	public void testAddTwoNumbersZeroTest() {
+		Assertions.assertEquals(0, sut.addTwoNumbers(0, 0));
+	}
+
+	@Test
+	public void testAddTwoNumbersPositive() {
+		Assertions.assertEquals(3, sut.addTwoNumbers(1, 2));
+	}
+
+	@Test
+	public void testAddTwoNumbersNegative() {
+		Assertions.assertEquals(-7, sut.addTwoNumbers(-3, -4));
+	}
+
+	@Test
+	public void testSimpleTest() {
+		Assertions.assertTrue(true);
+	}
+	
+}
