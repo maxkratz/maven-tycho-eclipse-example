55b0f52a18ea17b2203282f53fe47850ae375927
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-10-06 11:29:43 +0200

    Adds some example JavaDoc + Maven and CI config

--- org.example.impl/src/org/example/impl/ExamplePluginImplementation.java
+++ org.example.impl/src/org/example/impl/ExamplePluginImplementation.java
@@ -2,14 +2,29 @@ package org.example.impl;
 
 public class ExamplePluginImplementation {
 
+	/**
+	 * Runs an example function that prints "Hello World!".
+	 * 
+	 * @param args Array of String arguments that will be ignored.
+	 */
 	public static void main(final String[] args) {
 		System.out.println("Hello World!");
 	}
-	
+
+	/**
+	 * Adds two integer numbers together.-
+	 * 
+	 * @param a First integer to add.
+	 * @param b Second integer to add.
+	 * @return Sum of both integers a and b.
+	 */
 	public int addTwoNumbers(final int a, final int b) {
 		return a + b;
 	}
-	
+
+	/**
+	 * Dummy method that tries to access a field of org.apache.commons.io.
+	 */
 	public void dummy() {
 		final char c = org.apache.commons.io.FilenameUtils.EXTENSION_SEPARATOR;
 	}
